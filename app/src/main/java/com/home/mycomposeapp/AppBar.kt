bbf6db13546ae6cc2c95e6700dcdb8928037b0c0
erinad <egri.dani.r@gmail.com>
2022-06-04 15:13:42 +0200

    Drawer complete

--- app/src/main/java/com/home/mycomposeapp/AppBar.kt
+++ app/src/main/java/com/home/mycomposeapp/AppBar.kt
@@ -7,7 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 
 @Composable
-fun AppBAr(
+fun AppBar(
     onNavigationClick: () -> Unit
 ) {
     TopAppBar(
--- app/src/main/java/com/home/mycomposeapp/MainActivity.kt
+++ app/src/main/java/com/home/mycomposeapp/MainActivity.kt
@@ -1,40 +1,78 @@
 package com.home.mycomposeapp
 
+import DrawerBody
+import DrawerHeader
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.home.mycomposeapp.ui.theme.MyComposeAppTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyComposeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                val scaffoldState = rememberScaffoldState()
+                val drawerScope = rememberCoroutineScope()
+                Scaffold(
+                    scaffoldState = scaffoldState,
+                    topBar = {
+                         AppBar(
+                             onNavigationClick = {
+                                 drawerScope.launch {
+                                     scaffoldState.drawerState.open()
+                                 }
+                             }
+                         )
+                    },
+                    drawerContent = {
+                        DrawerHeader()
+                        DrawerBody(
+                            items = listOf(
+                                MenuItem(
+                                    id = "home",
+                                    title = "Home",
+                                    contentDescription = "Home option",
+                                    icon = Icons.Default.Home
+                                ),
+                                MenuItem(
+                                    id = "settings",
+                                    title = "Settings",
+                                    contentDescription = "Settings option",
+                                    icon = Icons.Default.Settings
+                                ),
+                                MenuItem(
+                                    id = "account",
+                                    title = "Account",
+                                    contentDescription = "Account option",
+                                    icon = Icons.Default.AccountCircle
+                                )
+                            ),
+                            onItemClick = {
+                                Toast.makeText(
+                                    applicationContext,
+                                    "Clicked on ${it.title}",
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        )
+                    }
+                ) {
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyComposeAppTheme {
-        Greeting("Android")
     }
 }
